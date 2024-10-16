ab9da25216cfa593e6fd9c219f08161858336e90
Carliscoding <101809224+Carliscoding@users.noreply.github.com>
2024-10-16 10:52:40 +0700

    setup  vpc -> create public/private subnet, internet gateway, route table, setup default security group for vpc

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -10,3 +10,120 @@ resource "aws_instance" "instance1" {
     name = "my-demo-instance"
   }
 }
+# Tạo VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "my_vpc"
+  }
+}
+
+# Tạo Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "my_igw"
+  }
+}
+
+# Tạo Public Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-southeast-2a" # Thay đổi theo region của bạn
+
+  tags = {
+    Name = "public_subnet"
+  }
+}
+
+# Tạo Route Table cho Public Subnet
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public_rt"
+  }
+}
+
+# Gán Route Table cho Public Subnet
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Tạo Elastic IP cho NAT Gateway
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+# Tạo NAT Gateway cho Private Subnet
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "nat_gateway"
+  }
+}
+
+# Tạo Private Subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-southeast-2a"
+
+  tags = {
+    Name = "private_subnet"
+  }
+}
+
+# Tạo Route Table cho Private Subnet (với NAT Gateway)
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+
+  tags = {
+    Name = "private_rt"
+  }
+}
+
+# Gán Route Table cho Private Subnet
+resource "aws_route_table_association" "private_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+# Tạo Security Group mặc định cho VPC
+resource "aws_security_group" "default_sg" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "default_sg"
+  }
+}
